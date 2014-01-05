598fe101ff3d08103d8d61f4e4deaea261b111bb
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-05 20:21:15 +0100

    Create root config and clear routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Sociaror::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/help"
+  root to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
 
   resources :users
 

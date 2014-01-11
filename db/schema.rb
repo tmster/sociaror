957fc404d12ef53d74933cef0a25c366ddf5d2c2
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-11 22:48:33 +0100

    Make email unique in validation and in database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131227200636) do
+ActiveRecord::Schema.define(:version => 20140111214523) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(:version => 20131227200636) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end

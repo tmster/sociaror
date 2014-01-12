8df6cca78d10cd89b7d88ffa65acb6a8e345bae3
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-12 21:04:03 +0100

    Speedup test by setting low cost bcrypt

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,8 @@
 Sociaror::Application.configure do
+    # Speed up tests by lowering BCrypt's cost function.
+  require 'bcrypt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's

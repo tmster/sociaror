b67290c80285f8e5f7a49053cd50585c238b07eb
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-14 23:12:24 +0100

    Add create action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,12 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      render 'new'
+    end
+  end
 end

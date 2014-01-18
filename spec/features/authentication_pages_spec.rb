0e3aee11b38f07ea83fa184b86ecb9e3fc83b68b
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-18 13:05:53 +0100

    Create session controller with tests

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe "Authentication" do
-
+  let(:base_title) { "SociaRoR" }
   subject { page }
 
   describe "signin page" do
     before { visit signin_path }
 
     it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_title("#{base_title} | Sign in") }
   end
 end

4e0b6db1c4ebcf748e85033ff1e40ed7cc7d8886
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-18 16:56:47 +0100

    Add signin functionallity with tests

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Authentication" do
       it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
       describe "after visiting another page" do
-        before { click_link "Strona Głowna" }
+        before { click_link "Strona Glowna" }
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
@@ -35,6 +35,11 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
 end

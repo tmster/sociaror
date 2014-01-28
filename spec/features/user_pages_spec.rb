7f90b811cf3d2e334fa160a8a1cc1b3c02cf421b
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-28 22:52:00 +0100

    Create pagination in user listing

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -20,6 +20,19 @@ describe "User pages" do
         page.should have_selector('li', text: user.name)
       end
     end
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li', text: user.name)
+        end
+      end
+    end
   end     
 
   describe "profile page" do

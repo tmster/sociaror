ebbe09b7ae937deaf6ff5dbeaeb8c0773f4fa7f9
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-01-30 23:38:53 +0100

    Fix error in heroku

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Sociaror
     I18n.enforce_available_locales = false
     I18n.default_locale = :pl
 config.i18n.available_locales = [:pl,:en]
+config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

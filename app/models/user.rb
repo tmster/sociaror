20522d2049b8e8a7189e1b45e17c5ce4db6a5620
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-02-02 19:52:46 +0100

    Add new fields in user mode

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation, :first_name, :last_name
+  attr_accessible :name, :email, :password, :password_confirmation, :first_name, :last_name, :gender, :email_visibility
   has_secure_password
 
   before_save { |user| user.email = email.downcase }

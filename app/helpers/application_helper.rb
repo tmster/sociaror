622688d7055a904dea7300fdab478f8aafc3167b
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-02-03 19:27:11 +0100

    Add helper method to use redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def markdown(text)
+  options = {
+    :autolink => true,
+    :space_after_headers => true,
+    :no_intra_emphasis => true
+  }
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+  markdown.render(text).html_safe
+end
 end

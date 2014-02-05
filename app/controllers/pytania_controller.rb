20c1f7b66aa37e2d8c1b3841b0509f9081e79491
Tomasz Szkaradek <tmster.sz@gmail.com>
2014-02-05 11:50:09 +0100

    Create look and feel for q&a database

--- app/controllers/pytania_controller.rb
+++ app/controllers/pytania_controller.rb
@@ -1,83 +1,53 @@
 class PytaniaController < ApplicationController
-  # GET /pytania
-  # GET /pytania.json
+  before_filter :signed_in_user, only: [:new, :create]
+
   def index
-    @pytania = Pytanium.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @pytania }
-    end
+    @pytania = Pytanium.where(:status => 1).paginate(page: params[:page], :per_page => 5).order('id DESC')
   end
 
-  # GET /pytania/1
-  # GET /pytania/1.json
+
   def show
-    @pytanium = Pytanium.find(params[:id])
+    @pytania = Pytanium.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @pytanium }
-    end
   end
 
-  # GET /pytania/new
-  # GET /pytania/new.json
+
   def new
-    @pytanium = Pytanium.new
+    @pytania = Pytanium.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @pytanium }
-    end
   end
 
-  # GET /pytania/1/edit
+
   def edit
-    @pytanium = Pytanium.find(params[:id])
+    @pytania = Pytanium.find(params[:id])
   end
 
-  # POST /pytania
-  # POST /pytania.json
+
   def create
-    @pytanium = Pytanium.new(params[:pytanium])
+    @pytania = Pytanium.new(params[:pytanium])
+    @pytania.user_id = current_user.id
 
-    respond_to do |format|
-      if @pytanium.save
-        format.html { redirect_to @pytanium, notice: 'Pytanium was successfully created.' }
-        format.json { render json: @pytanium, status: :created, location: @pytanium }
+      if @pytania.save
+        flash[:success] = "Pytanie zostalo dodane"
+
+        redirect_to pytania_url 
       else
-        format.html { render action: "new" }
-        format.json { render json: @pytanium.errors, status: :unprocessable_entity }
+        render action: "new" 
       end
-    end
+    
   end
 
-  # PUT /pytania/1
-  # PUT /pytania/1.json
   def update
-    @pytanium = Pytanium.find(params[:id])
+    @pytanie = Pytanium.find(params[:id])
+    @pytania = Pytanium.new(params[:pytanium])
+    @pytania.user_id = current_user.id
+    @pytania.parent_pytania_id=@pytanie.id
+      if @pytania.save
+        flash[:success] = "Odpowiedz zostala dodana"
 
-    respond_to do |format|
-      if @pytanium.update_attributes(params[:pytanium])
-        format.html { redirect_to @pytanium, notice: 'Pytanium was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to pytanium_url(@pytanie)
       else
-        format.html { render action: "edit" }
-        format.json { render json: @pytanium.errors, status: :unprocessable_entity }
+        render action: "new" 
       end
-    end
-  end
-
-  # DELETE /pytania/1
-  # DELETE /pytania/1.json
-  def destroy
-    @pytanium = Pytanium.find(params[:id])
-    @pytanium.destroy
-
-    respond_to do |format|
-      format.html { redirect_to pytania_url }
-      format.json { head :no_content }
-    end
   end
 end
--- app/models/pytanium.rb
+++ app/models/pytanium.rb
@@ -1,3 +1,8 @@
 class Pytanium < ActiveRecord::Base
   attr_accessible :content, :parent_pytania_id, :status, :title, :user_id
+    belongs_to :user
+  belongs_to :parent, :class_name => "Pytanium", :foreign_key => "parent_pytania_id"
+
+  has_many :replies, :class_name => "Pytanium", :foreign_key => "parent_pytania_id"
+
 end
